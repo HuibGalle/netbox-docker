f036c5bfc5d9deb94a34a1efed9011215eab8f48
HuibGalle <gallehuib@gmail.com>
2025-10-23 21:23:20 +0200

    Update plugins.py

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,13 +1,7 @@
-# Add your plugins and plugin settings here.
-# Of course uncomment this file out.
+PLUGINS = ["netbox_secretstore"]
 
-# To learn how to build images with your required plugins
-# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
-
-# PLUGINS = ["netbox_bgp"]
-
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+ PLUGINS_CONFIG = {
+   "netbox_secretstore": {
+     ADD YOUR SETTINGS HERE
+   }
+ }
